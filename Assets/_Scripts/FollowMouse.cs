71e1a3d851b689adbbdb630bc98b94a880a479dc
Allan Lavell <alavell@gmail.com>
2013-10-19 14:27:29 -0300

    Follow dat mousse

---
+++ Assets/_Scripts/FollowMouse.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowMouse : MonoBehaviour {
+	float smooth = 15;
+	Vector3 targetPosition;
+	
+	void Update () {
+		var playerPlane = new Plane(Vector3.forward, transform.position);
+		
+		var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		var hitdist = 0.0f;
+ 
+		if (playerPlane.Raycast(ray, out hitdist)) {
+			var targetPoint = ray.GetPoint(hitdist);
+			targetPosition = new Vector3(targetPoint.x, targetPoint.y, 0);
+		}
+		
+		transform.position = Vector3.Lerp (transform.position, targetPosition, Time.deltaTime * smooth);
+	}
+}
